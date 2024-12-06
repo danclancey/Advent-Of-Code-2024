58e336ed13f23798e4ae56382c440476b8c42b15
Sif33r <dan.clancey@gmail.com>
2024-12-06 19:50:33 +0000

    start day6

---
+++ day06/day06p1.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+#include <fstream>
+#include <vector>
+#include <unordered_set>
+#include <string>
+#include <tuple>
+
+const std::vector<std::pair<int, int>> DIRECTIONS = {
+    {-1, 0},  // Up
+    {0, 1},   // Right 
+    {1, 0},   // Down 
+    {0, -1}   // Left
+};
+
+void parseInput(const std::string& filename, std::vector<std::string>& map, int& startRow, int& startCol, int& startDir);
+int simGuard(const std::vector<std::string>& map, int startRow, int startCol, int startDir);
+
+int main() {
+
+    return 0;
+}
+
+void parseInput(const std::string& filename, std::vector<std::string>& map, int& startRow, int& startCol, int& startDir) {
+
+}
+
+int simguard(const std::vector<std::string& map, int startRow, int startCol, int startDir) {
+
+}
+
